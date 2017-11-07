cc1abf2e9b0bb49a5846f900e133c09011651354
zhangaz1 <zhangaz1@hotmail.com>
2017-11-07 17:17:54 +0800

    remove unused codes

--- src/baidu/index.js
+++ src/baidu/index.js
@@ -4,14 +4,7 @@ let baiduWindow = null;
 let webContents = null;
 
 const preloadJs = 'D:/Workspace/MyGit/MyProjects/web-contents-demo/app/preload.js';
-
 const loginUrl = 'https://passport.baidu.com/v2/?login';
-// const loginUrl = 'https://passport.cnblogs.com/user/signin';
-
-// const preJs = 'bower_components/jquery/dist/jquery.js';
-// const jqueryPath = ['file://', __dirname, preJs].join('/');
-const jqueryPath = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-// const jqueryPath = 'https://10.10.0.20:8080/app/bower_components/jquery/dist/jquery.js';
 
 export const init = () => {
 	$('#open').click(openHandler);
