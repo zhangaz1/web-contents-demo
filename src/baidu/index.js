5d3fa339da96072bac28389ba7c5db23ff474b43
zhangaz1 <zhangaz1@hotmail.com>
2017-11-07 17:11:34 +0800

    remove unuse codes

--- src/baidu/index.js
+++ src/baidu/index.js
@@ -24,10 +24,7 @@ function openHandler() {
 	webContents = baiduWindow.webContents;
 	webContents.openDevTools();
 
-	// webContents.on('did-finish-load', login);
 	webContents.loadURL(loginUrl);
-
-	// baiduWindow = open(loginUrl);
 }
 
 function createWindow() {
@@ -42,94 +39,3 @@ function createWindow() {
 		}
 	});
 };
-
-function login() {
-	return Promise.resolve()
-		.then(() => webContents.executeJavaScript(replaceModule()))
-		.then(() => webContents.executeJavaScript(loadJQuery(), true))
-		.then(result => console.log(result))
-		.then(() => webContents.executeJavaScript(rollbackModule()))
-		.then(() => webContents.executeJavaScript(registerMessage()))
-		.then(() => webContents.executeJavaScript(loginCode()));
-}
-
-function loginCode() {
-	return `
-		$('#TANGRAM__PSP_3__userName').val('zhangaz_temp');
-		$('#TANGRAM__PSP_3__password').val('abc123456');
-
-		$('#TANGRAM__PSP_3__verifyCode').val('abc');
-		// $('#TANGRAM__PSP_3__submit').click();
-	`;
-}
-
-function replaceModule() {
-	return `
-        window.module = module;
-        module = undefined;
-	`;
-}
-
-function rollbackModule() {
-	return `
-        module = window.module;
-	`;
-}
-
-function loadJQuery() {
-	return loadScript(jqueryPath);
-}
-
-function registerMessage() {
-	const code = `
-		const ipcRenderer = require('electron').ipcRenderer;
-
-		ipcRenderer.on('message', (event, data) => {
-			console.Log('got message', data);
-		});
-
-		ipcRenderer.send('message', {
-			data: 123
-		});
-	`;
-
-	return addScript(code);
-}
-
-function loadScript(src) {
-	return `
-		var resolvePromise;
-		var result = new Promise(function(resolve, reject){
-			resolvePromise = resolve;
-		});
-		var script = document.createElement('script');
-		${initScriptBySrc(src)}
-		script.onload = function() {
-			console.Log('load script: ', \`${replaceQuotationMarks(src)}\`);
-			resolvePromise('jquery ready');
-		};
-		document.body.appendChild(script);
-		result;
-	`;
-}
-
-function addScript(code) {
-	return `
-		var script = document.createElement('script');
-		${initScriptByCode(code)}
-		document.body.appendChild(script);
-		console.Log('load script: ', \`${replaceQuotationMarks(code)}\`);
-	`;
-}
-
-function initScriptBySrc(src) {
-	return `script.src = \`${replaceQuotationMarks(src)}\``;
-}
-
-function initScriptByCode(code) {
-	return `script.innerHTML = \`${replaceQuotationMarks(code)}\``;
-}
-
-function replaceQuotationMarks(str) {
-	return str.replace(/(["'`])/g, `\\$1`)
-}
